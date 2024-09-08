55f09000f68ac51573577dd2e7f04ac6c513f548
Latidude Maps <latidude.maps@gmail.com>
2024-09-08 12:21:23 +0200

    -

--- js/script.js
+++ js/script.js
@@ -10,33 +10,3 @@ document.addEventListener('keydown', event => {
 console.log = function() {};
 console.warn = function() {};
 console.error = function() {};
-
-// Add this at the end of your existing script.js file
-
-function initializePopups() {
-    if (typeof window.markers !== 'undefined' && Array.isArray(window.markers)) {
-        window.markers.forEach(function(marker) {
-            var lat = marker.getLatLng().lat;
-            var lng = marker.getLatLng().lng;
-            var matchingData = window.coordinates.find(function(coord) {
-                return coord[0] === lat && coord[1] === lng;
-            });
-            if (matchingData) {
-                var popupContent = '<b>Name:</b> ' + matchingData[2] + '<br><b>Elevation:</b> ' + matchingData[3];
-                marker.bindPopup(popupContent);
-            }
-        });
-    }
-}
-
-// Modify your existing DOMContentLoaded event listener
-document.addEventListener('DOMContentLoaded', function() {
-    // Your existing code here
-
-    // Add this check for coordinates
-    if (typeof window.coordinates !== 'undefined') {
-        initializePopups();
-    } else {
-        window.addEventListener('coordinatesLoaded', initializePopups);
-    }
-});
