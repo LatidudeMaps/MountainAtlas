ac130974cbc396c800071b251066f733fb8f78b4
Latidude Maps <latidude.maps@gmail.com>
2024-09-08 11:55:48 +0200

    popup JS

--- js/script.js
+++ js/script.js
@@ -10,3 +10,30 @@ document.addEventListener('keydown', event => {
 console.log = function() {};
 console.warn = function() {};
 console.error = function() {};
+
+// In script.js
+function initializePopups() {
+    if (typeof window.markers !== 'undefined' && Array.isArray(window.markers)) {
+        window.markers.forEach(function(marker) {
+            var lat = marker.getLatLng().lat;
+            var lng = marker.getLatLng().lng;
+            var matchingData = window.coordinates.find(function(coord) {
+                return coord[0] === lat && coord[1] === lng;
+            });
+            if (matchingData) {
+                var popupContent = '<b>Name:</b> ' + matchingData[2] + '<br><b>Elevation:</b> ' + matchingData[3];
+                marker.bindPopup(popupContent);
+            }
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Check if coordinates are loaded, if not, wait for them
+    if (typeof window.coordinates !== 'undefined') {
+        initializePopups();
+    } else {
+        // Wait for coordinates to be loaded
+        window.addEventListener('coordinatesLoaded', initializePopups);
+    }
+});
